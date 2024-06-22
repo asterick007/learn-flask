82df68aa94ce4e640dee2455727c536982b0f4c4
asterick007 <123404579+asterick007@users.noreply.github.com>
2024-06-22 18:54:22 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Flask"
+    return render_template('home.html')
+  
+@app.route('/home')
+def home_world():
+    return 'Home Part'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
+  
